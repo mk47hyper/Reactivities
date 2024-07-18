b64c8b8ddecefd99ec9f64c2ff5514295c1a7e59
m.kirilov <m.kirilov@reply.com>
2024-07-18 15:35:54 +0100

    02 Housekeeping process begin.

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -22,6 +24,8 @@ builder.Services.AddCors(opt =>
     });
 });
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -12,7 +12,6 @@ function App() {
     })
   },[]);
   
-
   return (
     <div>
       <Header as="h2" icon="users" content="Reactivities"/>
