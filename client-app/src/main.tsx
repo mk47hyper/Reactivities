d221aed90a5cd7dd24fa598da64314f35ae1b3bc
m.kirilov <m.kirilov@reply.com>
2024-08-01 11:58:12 +0100

    Refactoring folder structure.

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
